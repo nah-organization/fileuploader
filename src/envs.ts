d9ede4cf190fbd4f90edac42acc885ffaadf9cde
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2022-12-04 14:28:00 +0900

    changed env

--- src/envs.ts
+++ src/envs.ts
@@ -1,4 +1,5 @@
 export const hostname = process.env.FILEUPLOADER_HOSTNAME ?? 'localhost';
+export const realPort = +(process.env.FILEUPLOADER_REAL_PORT ?? '') || 443;
 
 export const maxUploadSize = 2 * 1024 * 1024 * 1024; // 2GiB
 
